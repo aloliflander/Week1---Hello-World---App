c7b129d8b5e1f310e358a96e72f176cdb477af13
alo liflander <alo.liflander@gmail.com>
2022-02-02 17:01:04 +0200

    Create Hello World Program

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string UserNamer;
+            UserNamer = Console.ReadLine();
+            Console.WriteLine("Hello, " + UserNamer);
+            Console.Read();
+        }
+    }
+}
